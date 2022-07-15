d178ec4031cb97ce85267720ca62ef24e8e4ce8d
TonsnakeLin <lpbgytong@163.com>
2022-07-15 22:30:55 +0800

    add info
    Signed-off-by: TonsnakeLin <lpbgytong@163.com>

--- util/execdetails/execdetails.go
+++ util/execdetails/execdetails.go
@@ -211,7 +211,8 @@ func (d ExecDetails) String() string {
 	lockDetail := d.LockKeysDetail
 	if lockDetail != nil {
 		parts = append(parts, "lock_rpc_count"+": "+strconv.FormatInt(lockDetail.LockRPCCount, 10))
-		parts = append(parts, "lock_tikv_time"+": "+strconv.FormatInt(lockDetail.LockTiKVTime, 10))
+		// parts = append(parts, "lock_tikv_time"+": "+strconv.FormatInt(lockDetail.LockTiKVTime, 10))
+		parts = append(parts, "lock_tikv_time"+": "+strconv.FormatFloat(lockDetail.LockTiKVTime/1000000000.0, 'f', -1, 64))
 	}
 
 	return strings.Join(parts, " ")
